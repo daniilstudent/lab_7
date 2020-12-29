e57057fe40eda4c287949e92b394a15ae8de753f
daniilstudent <snayper.daniil00@mail.ru>
2020-12-29 09:41:13 +0300

    С Новым годом!!!

---
+++ individual_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Написать программу, которая считывает английский текст из файла и выводит его на экран,
+# заменив каждую первую букву слов, начинающихся с гласной буквы, на прописную.
+
+if __name__ == '__main__':
+    with open('textineng.txt', 'r') as f:
+        textineng = f.read()
+    sentences = textineng.split(" ")
+    A = ["A", "E", "I", "O", "U", "Y"]
+    eng_1 = ' '
+    for sentence in sentences:
+
+        if sentence[0] == "A":
+            sentence = sentence.replace('A', 'a')
+
+        if sentence[0] == "E":
+            sentence = sentence.replace('E', 'e')
+
+        if sentence[0] == "I":
+            sentence = sentence.replace('I', 'i')
+
+        if sentence[0] == "O":
+            sentence = sentence.replace('O', 'o')
+
+        if sentence[0] == "U":
+            sentence = sentence.replace('U', 'u')
+
+        if sentence[0] == "Y":
+            sentence = sentence.replace('Y', 'y')
+
+        print(sentence)
---
+++ individual_2.py
@@ -0,0 +1,105 @@
+  
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+
+# Условние задания
+# Использовать словарь, содержащий следующие ключи: название товара; название
+# магазина, в котором продается товар; стоимость товара в руб. Написать программу,
+# выполняющую следующие действия: ввод с клавиатуры данных в список, состоящий из
+# словарей заданной структуры; записи должны быть размещены в алфавитном порядке по
+# названиям магазинов; вывод на экран информации о товарах, продающихся в магазине,
+# название которого введено с клавиатуры; если такого магазина нет, выдать на дисплей
+# соответствующее сообщение.
+
+if __name__ == '__main__':
+    products = []
+
+    while True:
+        command = input(">>> ").lower()
+
+        if command == 'exit':
+            break
+
+        elif command == 'add':
+            name = input("Название товара? ")
+            shop = input("Название магазина? ")
+            coast = int(input("Введите его цену "))
+
+            product = {
+                'name': name,
+                'shop': shop,
+                'coast': coast,
+            }
+
+            products.append(product)
+            if len(product) > 1:
+                products.sort(key=lambda item: item.get('shop', ''))
+
+        elif command == 'list':
+            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+                '-' * 4,
+                '-' * 30,
+                '-' * 20,
+                '-' * 8
+            )
+            print(line)
+            print(
+                ' | {:^4} | {:^30} | {:^20} | {:^8} |'.format(
+                    "№",
+                    "Наименование товара",
+                    "Название магазина",
+                    "Стоимость"
+                )
+            )
+            print(line)
+
+            for idx, product in enumerate(products, 1):
+                print(
+                    '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
+                        idx,
+                        product.get('name', ''),
+                        product.get('shop', ''),
+                        product.get('coast', 0)
+                    )
+                )
+
+            print(line)
+
+        elif command.startswith('select '):
+            parts = command.split(' ', maxsplit=1)
+            count = 0
+
+            for product in products:
+                if parts[1] == product.get('shop').lower():
+                    count += 1
+                    print(
+                        '{:>4}: {} {}'.format(count, product.get('coast', ' '), product.get('name', ' '))
+                    )
+                if count == 0:
+                    print("Магазин с таким названием не найден")
+
+        elif command.startswith('load '):
+            parts = command.split(' ', maxsplit=1)
+            with open(parts[1], 'r') as f:
+                products = json.load(f)
+
+        elif command.startswith('save '):
+            parts = command.split(' ', maxsplit=1)
+            with open(parts[1], 'w') as f:
+                json.dump(products, f)
+
+        elif command == 'help':
+            # Вывести справку о работе с программой.
+            print("Список команд:\n")
+            print("add - добавить товары;")
+            print("list - вывести список товаров;")
+            print("select <магазин> - запросить товары в выбранном магазине")
+            print("load <имя файла> - загрузить данные из файла;")
+            print("save <имя файла> - сохранить данные в файл;")
+            print("help - отобразить справку;")
+            print("exit - завершить работу с программой.")
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
